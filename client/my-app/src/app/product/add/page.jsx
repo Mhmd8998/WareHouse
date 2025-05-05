9d58661a7902af74d573af02355efbf9726e409e
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-05-05 12:44:46 +0300

    Create page.jsx

---
+++ client/my-app/src/app/product/add/page.jsx
@@ -0,0 +1,72 @@
+"use client"
+import { useState } from 'react';
+
+export default function Login() {
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [status, setStatus] = useState('');
+  const [message, setMessage] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+     const storedToken = localStorage.getItem('token');
+     if (storedToken) setToken(storedToken);
+  }, []);
+
+  const handleAddProduct = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch('http://localhost:8000/api/product/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify({ name, status, quantity }),
+    });
+
+    const data = await res.json();
+    
+    setMessage(data.message);
+
+    
+  };
+
+  return (
+    <form onSubmit={handleAddProduct} className='container w-75 my-5'>
+      <h2>اضافة منتج</h2>
+      <div className="mb-3">
+        <label className="form-label">اسم المنتج</label>
+        <input
+          type="text"
+          className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">الحالة</label>
+        <input
+          type="text"
+          className="form-control"
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">الكمية</label>
+        <input
+          type="number"
+          className="form-control"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary w-100">اضافة</button>
+      {message && <div className="mt-3 alert alert-info">{message}</div>}
+    </form>
+  );
+  }

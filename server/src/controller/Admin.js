2001bda1b98c2247b1b857cd2d2da2966b760933
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-05-19 16:27:43 +0300

    Create Admin.js

---
+++ server/src/controller/Admin.js
@@ -0,0 +1,35 @@
+const db = require('../DB/db');
+const asyncHandler = require("express-async-handler");
+
+module.exports = {
+    getAdminNotification: asyncHandler(async (req, res) => {
+        // تحقق من كون المستخدم مسؤول
+        
+
+        try {
+            const result = await new Promise((resolve, reject) => {
+                db.all(
+                    "SELECT * FROM notifications ORDER BY created_at DESC",
+                    [],
+                    (err, rows) => {
+                        if (err) reject(err);
+                        else resolve(rows);
+                    }
+                );
+            });
+
+            if (result.length === 0) {
+                return res.status(404).json({ message: "لم يتم العثور على إشعارات." });
+            }
+
+            return res.status(200).json({
+                count: result.length,
+                notifications: result
+            });
+
+        } catch (error) {
+            console.error("Database error:", error);
+            return res.status(500).json({ message: "حدث خطأ في قاعدة البيانات." });
+        }
+    }),
+};

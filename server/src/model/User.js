7b0cafce1a949cca321abbf70182e76f872d38db
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-05-19 15:18:37 +0300

    Update User.js

--- server/src/model/User.js
+++ server/src/model/User.js
@@ -3,8 +3,17 @@ const joi = require('joi');
 db.run(`CREATE TABLE IF NOT EXISTS user(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
+        password TEXT NOT NULL,
+        isAdmin BOOLEAN DEFAULT 0
     )`);
+db.run(`CREATE TABLE notifications (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  message TEXT,
+  user_id INTEGER,
+  read INTEGER DEFAULT 0,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)`);
     
     const validateRegisterUser = (obj) => {
         const schema = joi.object({
